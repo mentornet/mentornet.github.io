ccea5e83b5673cd9e1b8395fa15a4db6e6bd639f
Mentor Net <tom@joinmentornet.me>
2018-05-01 12:15:13 +1000

    Update

--- js/session-data.js
+++ js/session-data.js
@@ -1,45 +1,55 @@
-const sessionKeys = [
-	'payid',
-	'name',
-	'phone',
-	'skills',
-	'credit',
-	'uid',
-	'sessions',
-	'isActive'
-];
+const sessionManager = new (function () {
+	const sessionKeys = [
+		'uid',
 
-let data = {};
+		'email',
 
-sessionKeys.forEach(key => data[key] = JSON.parse(sessionStorage.getItem(key)));
+		'name',
+		'phone',
 
-// build session data
-function getSessionData () {
-	return http.get('/auth')
-		.then(response => {
-			buildSessionData(response.data);
+		'currentSession',
+		'pastSessions',
+		'comingSessions',
+
+		'skills',
+		'isActive'
+	];
+
+	let data = {};
+
+	sessionKeys.forEach(key => data[key] = JSON.parse(sessionStorage.getItem(key)));
+
+	// build session data
+	this.getSessionData = function getSessionData () {
+		return http.get('/auth')
+			.then(response => {
+				buildSessionData(response.data);
+			});
+	};
+
+	this.setDataFromModel = function buildSessionData (dataRef) {
+		Object.keys(dataRef).forEach(key => {
+			let value = dataRef[key];
+			setSessionKey(key, value);
 		});
-}
+	}
 
-function buildSessionData (dataRef) {
-	Object.keys(dataRef).forEach(key => {
-		let value = dataRef[key];
-		setSessionKey(key, value);
-	});
-}
+	function setSessionKey (key, value) {
+		data[key] = value;
+		sessionStorage.setItem(key, JSON.stringify(value));
+	}
 
-function setSessionKey (key, value) {
-	data[key] = value;
-	sessionStorage.setItem(key, JSON.stringify(value));
-}
+	this.getDataKey = (key) => data[key];
+	this.getDataSubset = (...keys) => keys.reduce((a,b) => (a[b] = data[b], a), {});
 
-function clearSessionData () {
-	Object.keys(sessionKeys).forEach(key => {
-		data[key] = null;
-	});
-	sessionStorage.clear();
-}
+	this.clearData = function clearSessionData () {
+		Object.keys(sessionKeys).forEach(key => {
+			data[key] = null;
+		});
+		sessionStorage.clear();
+	}
 
-function hasSessionData () {
-	return data.payid !== null;
-}
+	this.hasData = function hasSessionData () {
+		return data.email !== null;
+	}
+});
